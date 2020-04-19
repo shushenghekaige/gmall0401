7e04eb63ec207e75a60f31928d8ecf4ec6c9f588
shushenghekaige <13636389663@163.com>
2020-04-19 23:50:23 +0800

    搭建dubbo分布式服务

---
+++ gmall-user-service/src/main/java/com/atguigu/gmall/user/service/Imp/UserServiceImp.java
@@ -0,0 +1,47 @@
+package com.atguigu.gmall.user.service.Imp;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.atguigu.gmall.bean.UmsMember;
+import com.atguigu.gmall.bean.UmsMemberReceiveAddress;
+import com.atguigu.gmall.service.UserService;
+import com.atguigu.gmall.user.mapper.UmsMemberReceiveAddressMapper;
+import com.atguigu.gmall.user.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.List;
+
+@Service
+public class UserServiceImp implements UserService {
+
+    @Autowired
+    UserMapper userMapper;
+
+    @Autowired
+    UmsMemberReceiveAddressMapper umsMemberReceiveAddressMapper;
+
+    @Override
+    public List<UmsMember> getAll() {
+
+        List<UmsMember> umsMembers=userMapper.selectAll();
+
+        return umsMembers;
+    }
+
+    @Override
+    public List<UmsMemberReceiveAddress> getAddressByMemberId(String memberId) {
+
+//        UmsMemberReceiveAddress umsMemberReceiveAddress = new UmsMemberReceiveAddress();
+//        umsMemberReceiveAddress.setMemberId(memberId);
+//
+//        List<UmsMemberReceiveAddress> umsMemberReceiveAddresses = umsMemberReceiveAddressMapper.select(umsMemberReceiveAddress);
+
+        Example example = new Example(UmsMemberReceiveAddress.class);
+        example.createCriteria().andEqualTo("memberId",memberId);
+
+        List<UmsMemberReceiveAddress> umsMemberReceiveAddresses = umsMemberReceiveAddressMapper.selectByExample(example);
+
+        return umsMemberReceiveAddresses;
+    }
+}
---
+++ gmall-user-web/src/main/java/com/atguigu/gmall/user/controller/UserController.java
@@ -0,0 +1,58 @@
+package com.atguigu.gmall.user.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.atguigu.gmall.bean.UmsMember;
+import com.atguigu.gmall.bean.UmsMemberReceiveAddress;
+import com.atguigu.gmall.service.UserAddressService;
+import com.atguigu.gmall.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+
+
+    @Reference
+    UserService userService;
+
+
+
+    @RequestMapping("getAll")
+    @ResponseBody
+    public List<UmsMember> getAllUser(){
+        List<UmsMember> umsMembers=userService.getAll();
+
+        return umsMembers;
+    }
+
+    @RequestMapping("getAddressByMemberId")
+    @ResponseBody
+    public List<UmsMemberReceiveAddress> getAddressByMemberId(String memberId){
+
+        List<UmsMemberReceiveAddress> umsMemberReceiveAddresses=userService.getAddressByMemberId(memberId);
+
+        return umsMemberReceiveAddresses;
+
+    }
+
+
+    @Reference
+    UserAddressService userAddressService;
+
+    @RequestMapping("getAddress")
+    @ResponseBody
+    public List<UmsMemberReceiveAddress> getAddress(String memberId){
+
+        List<UmsMemberReceiveAddress> umsMemberReceiveAddressess=userAddressService.getAddress(memberId);
+
+        return umsMemberReceiveAddressess;
+    }
+
+
+
+
+}
